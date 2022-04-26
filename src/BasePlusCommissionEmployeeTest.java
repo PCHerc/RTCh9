14d24ed70f0f4aa401913315db4c9b86912b52bd
rtalbott <Robert.Talbott@Gmail.com>
2022-04-26 10:34:30 -0500

    Edited the comments to see if changes are transmitting.

--- src/BasePlusCommissionEmployeeTest.java
+++ src/BasePlusCommissionEmployeeTest.java
@@ -3,7 +3,9 @@
 
 public class BasePlusCommissionEmployeeTest {
     public static void main(String[] args) {
+
         // instantiate BasePlusCommissionEmployee object
+        //
         BasePlusCommissionEmployee employee =
                 new BasePlusCommissionEmployee(
                         "Bob", "Lewis", "333-33-3333", 5000, .04, 300);
